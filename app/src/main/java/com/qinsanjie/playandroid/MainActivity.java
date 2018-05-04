93e35c8c77e414abb6ef22bb26a2bc327189d378
qsj <qsj_android@163.com>
2018-05-05 00:28:15 +0800

    添加okhttp,glide，基本工具库

--- app/src/main/java/com/qinsanjie/playandroid/MainActivity.java
+++ app/src/main/java/com/qinsanjie/playandroid/MainActivity.java
@@ -3,11 +3,14 @@ package com.qinsanjie.playandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bumptech.glide.module.AppGlideModule;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ lib_base/src/main/java/com/qinsanjie/libbase/imageloader/BaseGlideModule.java
@@ -0,0 +1,12 @@
+package com.qinsanjie.libbase.imageloader;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.module.LibraryGlideModule;
+
+/**
+ * Created by qsj_a on 2018/5/5.
+ */
+@GlideModule
+public class BaseGlideModule extends AppGlideModule {
+}
